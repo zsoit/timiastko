2e977662482ea25d39bc7b216efe2f5a97b94d34
wsparcie.exe@gmail.com <wsparcie.exe@gmail.com>
2020-09-18 23:50:56 +0200

    Update files

---
+++ zadanie1/app/index.js
@@ -0,0 +1,130 @@
+function falseA1() {
+    document.getElementById("trueOrfalse1").innerHTML = "Zła odpowiedź! <img src=https://myzsoit.pl/img/nav/error.png width=20px>";
+    document.title = "Zła odpowiedź!"
+
+}
+
+
+function trueB1() {
+    document.getElementById("trueOrfalse1").innerHTML = "Poprawna odpowiedź! <img src=https://myzsoit.pl/img/nav/yes.png width=20px>";
+    document.title = "Poprawna odpowiedź!";
+
+}
+
+function falseA2() {
+    document.getElementById("trueOrfalse2").innerHTML = "Spróbój ponownie! <img src=https://myzsoit.pl/img/nav/error.png width=20px>";
+    document.title = "Zła odpowiedź!"
+
+}
+
+function trueB2() {
+    document.getElementById("trueOrfalse2").innerHTML = "Dobrze! <img src=https://myzsoit.pl/img/nav/yes.png width=20px>";
+    document.title = "Poprawna odpowiedź!";
+
+}
+
+function falseA3() {
+    document.getElementById("trueOrfalse3").innerHTML = "Zła odpowiedź! <img src=https://myzsoit.pl/img/nav/error.png width=20px>";
+    document.title = "Zła odpowiedź!"
+
+}
+
+function trueB3() {
+    document.getElementById("trueOrfalse3").innerHTML = "Odpowiedziałeś dobrze! <img src=https://myzsoit.pl/img/nav/yes.png width=20px>";
+    document.title = "Poprawna odpowiedź!";
+
+}
+
+function falseA4() {
+    document.getElementById("trueOrfalse4").innerHTML = "Zła odpowiedź! <img src=https://myzsoit.pl/img/nav/error.png width=20px>";
+    document.title = "Zła odpowiedź!"
+
+}
+
+function trueB4() {
+    document.getElementById("trueOrfalse4").innerHTML = "Poprawna odpowiedź! <img src=https://myzsoit.pl/img/nav/yes.png width=20px>";
+    document.title = "Dokładnie!";
+
+}
+
+function falseA5() {
+    document.getElementById("trueOrfalse5").innerHTML = "Otóż nie tym razem! <img src=https://myzsoit.pl/img/nav/error.png width=20px>";
+    document.title = "Zła odpowiedź!"
+
+}
+
+function trueB5() {
+    document.getElementById("trueOrfalse5").innerHTML = "Poprawna odpowiedź! <img src=https://myzsoit.pl/img/nav/yes.png width=20px>";
+    document.title = "Poprawna opowiedź!";
+
+}
+
+function falseA6() {
+    document.getElementById("trueOrfalse6").innerHTML = "Zła odpowiedź! <img src=https://myzsoit.pl/img/nav/error.png width=20px>";
+    document.title = "Zła odpowiedź!"
+
+}
+
+function trueB6() {
+    document.getElementById("trueOrfalse6").innerHTML = "Jak najbardziej! <img src=https://myzsoit.pl/img/nav/yes.png width=20px>";
+    document.title = "Poprawna odpowiedź!";
+
+}
+
+function falseA7() {
+    document.getElementById("trueOrfalse7").innerHTML = "Zła odpowiedź! <img src=https://myzsoit.pl/img/nav/error.png width=20px>";
+    document.title = "Zła odpowiedź!"
+
+}
+
+function trueB7() {
+    document.getElementById("trueOrfalse7").innerHTML = "Tak trzymaj! <img src=https://myzsoit.pl/img/nav/yes.png width=20px>";
+    document.title = "Poprawna odpowiedź!";
+
+}
+
+function falseA8() {
+    document.getElementById("trueOrfalse8").innerHTML = "Zła odpowiedź! <img src=https://myzsoit.pl/img/nav/error.png width=20px>";
+    document.title = "Zła odpowiedź!"
+
+}
+
+function trueB8() {
+    document.getElementById("trueOrfalse8").innerHTML = "Poprawna odpowiedź! <img src=https://myzsoit.pl/img/nav/yes.png width=20px>";
+    document.title = "Poprawna odpowiedź!";
+
+}
+
+function falseA9() {
+    document.getElementById("trueOrfalse9").innerHTML = "Zła odpowiedź! <img src=https://myzsoit.pl/img/nav/error.png width=20px>";
+    document.title = "Zła odpowiedź!"
+
+}
+
+function trueB9() {
+    document.getElementById("trueOrfalse9").innerHTML = "Dobrze Ci idzie! <img src=https://myzsoit.pl/img/nav/yes.png width=20px>";
+    document.title = "Poprawna odpowiedź!";
+
+}
+
+function falseA10() {
+    document.getElementById("trueOrfalse10").innerHTML = "Zła odpowiedź! <img src=https://myzsoit.pl/img/nav/error.png width=20px>";
+    document.title = "Zła odpowiedź!"
+
+}
+
+function trueB10() {
+    document.getElementById("trueOrfalse10").innerHTML = "Jeszcze jak! <img src=https://myzsoit.pl/img/nav/yes.png width=20px>";
+    document.title = "Poprawna odpowiedź!";
+
+}
+
+
+
+
+
+function showEgg() {
+    document.getElementById('showEgg').style.display = "block";
+    document.getElementById('main').style.display = "none";
+
+}
